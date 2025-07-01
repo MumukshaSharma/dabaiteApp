1ab1dc50710adf731fe8d6e2b2e303a80adfcee3
tanishka <tanishkajain081@gmail.com>
2025-07-02 00:25:36 +0530

    name changed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Welcome to Dabite')),
+      appBar: AppBar(title: Text('Welcome to mumuksha')),
       body: Center(child: Text('Your meal plan goes here!')),
     );
   }
