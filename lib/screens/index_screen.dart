159c7810df1806ba3e833764547e91cf31c33b3a
tanishka <tanishkajain081@gmail.com>
2025-07-04 02:44:24 +0530

    itemss added

--- lib/screens/index_screen.dart
+++ lib/screens/index_screen.dart
@@ -21,41 +21,9 @@ class _IndexScreenState extends State<IndexScreen> {
     } else if (index == 2) {
       Navigator.pushNamed(context, '/kitchens');
     }
-
-    if (index == 1) {
-      Navigator.pushNamed(context, '/subscribe');
-    } else if (index == 2) {
-      Navigator.pushNamed(context, '/kitchens');
-    }
   }
 
   Widget _buildCategoryCard(String title, String imagePath, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(
-              imagePath,
-              height: 80,
-              width: 80,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(height: 6),
-          Text(
-            title,
-            style: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 14,
-              color: Color(0xFF4E6746),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
     return GestureDetector(
       onTap: onTap,
       child: Column(
@@ -137,32 +105,6 @@ class _IndexScreenState extends State<IndexScreen> {
                       ),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      _buildCategoryCard(
-                        'Students',
-                        'assets/icons/students.jpg',
-                        () {
-                          Navigator.pushNamed(context, '/student-plan');
-                        },
-                      ),
-                      _buildCategoryCard(
-                        'Elderly',
-                        'assets/icons/elderly.jpg',
-                        () {
-                          Navigator.pushNamed(context, '/elderly-plan');
-                        },
-                      ),
-                      _buildCategoryCard(
-                        'Corporate',
-                        'assets/icons/corporate.avif',
-                        () {
-                          Navigator.pushNamed(context, '/corporate-plan');
-                        },
-                      ),
-                    ],
-                  ),
                 ],
               ),
             ),
@@ -175,7 +117,6 @@ class _IndexScreenState extends State<IndexScreen> {
               child: Column(
                 children: const [
                   Text(
-                    'Welcome to Dabite',
                     'Welcome to Dabite',
                     style: TextStyle(
                       fontSize: 20,
@@ -200,8 +141,6 @@ class _IndexScreenState extends State<IndexScreen> {
           ],
         ),
       ),
-
-      // Bottom Navigation Bar
 
       // Bottom Navigation Bar
       bottomNavigationBar: BottomNavigationBar(
